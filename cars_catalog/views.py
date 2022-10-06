fdf7c7c0906456dfc0dd0b1f7d19e1184ead219c
lefriik <emerio.isaac.suarez@gmail.com>
2022-10-05 22:01:21 -0300

    Add car_list

--- cars_catalog/views.py
+++ cars_catalog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Car
 
 # Create your views here.
 
 def index(request):
+    cars = Car.objects.all() 
 
-    return render(request, "layout/base.html")
+    return render(request, 'cars_catalog/car_list.html', {'car_list' : cars})
     
+
+
