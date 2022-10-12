7788bbd2a5342bc3c59cbf4f6cf048941cf195ae
lefriik <emerio.isaac.suarez@gmail.com>
2022-10-12 12:45:47 -0300

    cars image added

--- cars_catalog/models.py
+++ cars_catalog/models.py
@@ -6,3 +6,4 @@ class Car(models.Model):
   plate = models.CharField(max_length = 8, unique = True)
   brand = models.CharField(max_length = 25)
   color = models.CharField(max_length = 30)
+  imagen = models.ImageField(default='null', verbose_name="miniatura", upload_to="autos")
