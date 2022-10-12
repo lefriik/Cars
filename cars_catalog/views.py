3e7e2737280ec61cd8aa0d6bd34c5063deb85750
lefriik <emerio.isaac.suarez@gmail.com>
2022-10-12 14:50:58 -0300

    car_details updated

--- cars_catalog/views.py
+++ cars_catalog/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Car
 
 # Create your views here.
 
-def index(request):
+def car_list(request):
     cars = Car.objects.all() 
 
     return render(request, 'cars_catalog/car_list.html', {'car_list' : cars})
     
 
+def car_detail(request, pk):
+
+    car= get_object_or_404(Car, pk=pk)
+
+    return render(request, 'cars_catalog/car_detail.html', { 'car': car} )
 
